d53b259c748f4f3c9bfb3ee2745d90e5452cfad3
Souvede Inshuti <77120594+InshutiSouvede@users.noreply.github.com>
2024-03-26 11:14:48 +0200

    Update 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-"""Script that fetches https://alx-intranet.hbtn.io/status."""
-from urllib.request import Request, urlopen
+"""A script that
+- fetches https://alu-intranet.hbtn.io/status.
+- uses urlib package
+"""
+
+import urllib.request
 
 
-if __name__ == "__main__":
-    # alx-https://intranet.hbtn.io
-    req = Request("https://intranet.hbtn.io/status")
-    with urlopen(req) as response:
-        body = response.read()
+if __name__ == '__main__':
+    url = 'https://intranet.hbtn.io/status'
+    if url.startswith('https://'):
+        url = "https://alu-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as res:
+        content = res.read()
         print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
